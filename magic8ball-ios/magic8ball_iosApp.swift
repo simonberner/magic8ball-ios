9c9d50f051d30cbc5e570ca6e33ebb2fc03361d3
Simon Berner <sibebzh@gmail.com>
2021-02-11 20:17:23 +0100

    init: :tada: initialize the project

---
+++ magic8ball-ios/magic8ball_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  magic8ball_iosApp.swift
+//  magic8ball-ios
+//
+//  Created by Simon Berner on 11.02.21.
+//
+
+import SwiftUI
+
+@main
+struct magic8ball_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
